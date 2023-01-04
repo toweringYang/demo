2b5641a1df957f0eec7808d1191af86f54e4a9b6
tower yang <1664496696@qq.com>
2023-01-04 16:32:38 +0800

    fixsomething

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,12 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author yangchongxiao
+ */
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class Demo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "hello world";
     }
 
 }
